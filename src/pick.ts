0927d5576d75ec9cc06f22d5eecc50d002e49705
Thomas Fike <Fike@me.com>
2022-10-10 21:05:59 -0700

    Only offer valuable push options.
    Change-Id: Ia6b73de2ef26b9f21543178e725d7580e282dcd3

--- src/pick.ts
+++ src/pick.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 import { CURRENT_BRANCH_ICON, settings } from './extension';
-import { PushFlagsPickOptions } from './types';
+import { PushFlagsPickOptions, PushFlags } from './types';
 import * as utils from './utils';
 
 export function makeBranchesQuickPick(branches: string[]): vscode.QuickPickItem[] {
@@ -19,11 +19,34 @@ export function makeBranchesQuickPick(branches: string[]): vscode.QuickPickItem[
 export function makePushFlagsQuickPick(): vscode.QuickPickItem[] {
     let list: vscode.QuickPickItem[] = [];
     for (let [option, enumVal] of PushFlagsPickOptions) {
-        if (enumVal === settings.push_defaultPushOption) {
-            list.splice(0, 0, { label: option });
-        } else {
-            list.push({ label: option });
+        if (pushFlagValid(enumVal)) {
+            if (enumVal === settings.push_defaultPushOption) {
+                list.splice(0, 0, { label: option });
+            } else {
+                list.push({ label: option });
+            }
         }
     }
     return list;
+}
+
+function pushFlagValid(enumVal: PushFlags): boolean {
+    console.log(utils.lastQuerry);
+    switch (enumVal) {
+        case PushFlags.NONE:
+            return true;
+        case PushFlags.WIP:
+            return utils.lastQuerry.wip === false;
+        case PushFlags.READY:
+            return utils.lastQuerry.wip === true;
+        case PushFlags.PRIVATE:
+            return utils.lastQuerry.private === false;
+        case PushFlags.REMOVE_PRIVATE:
+            return utils.lastQuerry.private === true;
+        default:
+            console.log("error");
+            return false;
+            break;
+    }
+
 }
